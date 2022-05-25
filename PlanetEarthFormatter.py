458fd965265b8b75440dfa4398507b0be63a255d
vdymsh <vdymsh@gmail.com>
2022-05-25 16:00:37 +0300

    remove old files from AllEnglishWords

--- PlanetEarthFormatter.py
+++
@@ -1,9 +0,0 @@
-fout = open("PlanetEarth_1.txt", "w")
-with open("PlanetEarth_1.srt", "r") as fin:
-    for line in fin:
-        line = line.strip()
-        if len(line) == 0 or ':' in line[:2]:
-            continue
-        fout.write(line + '\n')
-print("All done!")
-fout.close()
